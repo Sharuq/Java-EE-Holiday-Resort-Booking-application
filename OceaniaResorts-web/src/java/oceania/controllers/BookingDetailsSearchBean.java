3a92a664fb30a2604febd8d32ec73dd10c221106
sharuq <mkun0005@student.monash.edu>
2018-09-20 03:05:15 +1000

    second commit

--- OceaniaResorts-web/src/java/oceania/controllers/BookingDetailsSearchBean.java
+++ OceaniaResorts-web/src/java/oceania/controllers/BookingDetailsSearchBean.java
@@ -5,9 +5,15 @@
  */
 package oceania.controllers;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import oceania.entities.Booking;
 import oceania.search.BookingDetailsSearch;
 
 /**
@@ -16,12 +22,69 @@ import oceania.search.BookingDetailsSearch;
  */
 @Named(value = "bookingDetailsSearchBean")
 @RequestScoped
-public class BookingDetailsSearchBean {
+public class BookingDetailsSearchBean implements Serializable {
 
     @EJB
     private BookingDetailsSearch bookingDetailsSearch;
+   
+    private String optionSelected;
+    private String searchKeyword;
+    private List<Booking> searchResultList;
     
-    public BookingDetailsSearchBean() {
+    
+     public BookingDetailsSearchBean()  {
+         
+     }
+     public void searchBooking() {
+        
+         try {
+        // searchResultList = new ArrayList<>();
+         if(optionSelected.equals("Booking Number"))
+         {
+             searchResultList =  bookingDetailsSearch.searchByBookingNo(Integer.parseInt(searchKeyword));
+             
+         }
+         else if(optionSelected.equals("Booking Type"))
+         {
+             searchResultList =  bookingDetailsSearch.searchByBookingType(searchKeyword);
+             
+         }
+         else 
+         {
+             searchResultList =  bookingDetailsSearch.searchByBookingName(searchKeyword);
+             
+         }
+         }
+         catch (Exception ex) {
+                 Logger.getLogger(BookingDetailsSearchBean.class.getName()).log(Level.SEVERE, null, ex);
+             }
+     }
+         
+    
+    
+    public String getOptionSelected() {
+        return optionSelected;
+    }
+
+    public void setOptionSelected(String optionSelected) {
+        this.optionSelected = optionSelected;
+    }
+
+    public String getSearchKeyword() {
+        return searchKeyword;
+    }
+
+    public void setSearchKeyword(String searchKeyword) {
+        this.searchKeyword = searchKeyword;
+    }
+
+    public List<Booking> getSearchResultList() {
+        return searchResultList;
+    }
+
+    public void setSearchResultList(ArrayList<Booking> searchResultList) {
+        this.searchResultList = searchResultList;
     }
     
+  
 }
